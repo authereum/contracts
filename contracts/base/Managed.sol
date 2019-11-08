d38d3158ec103def236d9e91dbc89f040d8e09df
Chris Whinfrey <cwhinfrey@gmail.com>
2019-11-08 10:28:55 -0800

    Add GNU License for Argent contracts

--- contracts/base/Managed.sol
+++ contracts/base/Managed.sol
@@ -1,9 +1,28 @@
+/*
+       Copyright (C) 2018 Argent Ltd. <https://argent.xyz>
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 pragma solidity ^0.5.8;
 
 import "./Owned.sol";
 
 /**
  * @title Managed
+ * @author Julien Niset - <julien@argent.im>
  * @dev Basic contract that defines a set of managers. Only the owner can add/remove managers.
  */
 
--- contracts/ens/AuthereumENSResolver.sol
+++ contracts/ens/AuthereumENSResolver.sol
@@ -1,3 +1,21 @@
+/*
+       Copyright (C) 2018 Argent Ltd. <https://argent.xyz>
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 pragma solidity ^0.5.8;
 
 import "../base/Owned.sol";
@@ -6,6 +24,7 @@ import "./ENS.sol";
 
 /**
  * @title AuthereumEnsResolver
+ * @author Julien Niset - <julien@argent.im>
  * @dev Authereum implementation of a Resolver.
  */
 
